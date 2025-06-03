daada73113c527331c9911223dd2d18760f604d9
Nylo <langnylo1@gmail.com>
2025-06-03 13:43:53 -0300

    main.py
    Onde roda tudo

---
+++ main.py
@@ -0,0 +1,10 @@
+from lexico import lexer
+from sintatico import Parser
+
+if __name__ == "__main__":
+	with open("codigo.nylo", "r") as f:
+		codigo = f.read()
+
+	tokens = lexer(codigo)
+	parser = Parser(tokens)
+	parser.parse()
