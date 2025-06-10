7d6abfff64ad7377f062ca9b5a063f42c5f7edff
Breno Barbosa <eu.breno.oliveira.7@gmail.com>
2025-06-10 16:55:32 -0300

    Update main.py
    Correção do main.py

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ from lexico import lexer
 from sintatico import Parser
 
 if __name__ == "__main__":
-	with open("codigo.nylo", "r") as f:
-		codigo = f.read()
+    with open("codigo.nylo", "r") as f:
+        codigo = f.read()
 
-	tokens = lexer(codigo)
-	parser = Parser(tokens)
-	parser.parse()
+    tokens = lexer(codigo)
+    parser = Parser(tokens)
+    parser.parse()
